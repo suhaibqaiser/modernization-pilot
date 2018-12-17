eab28b5652cd53bee1b08205e5d2a0a214ea4b4a
Suhaib Qaiser <sqaiser@architech.ca>
2018-12-17 13:59:34 -0500

    Pushing skeleton piece to pilot project

---
+++ rr-content-layer/src/test/java/com/rotorooter/businessservice/rrcontentlayer/RrContentLayerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rotorooter.businessservice.rrcontentlayer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RrContentLayerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
