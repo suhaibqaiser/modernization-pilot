ac5ca246397cc42fd6082fe1abed4e1a183836d1
Suhaib Qaiser <sqaiser@architech.ca>
2018-12-18 10:48:13 -0500

    finishing up the container implementation

---
+++ rr-content-layer/src/main/java/com/rotorooter/businessservice/rrcontentlayer/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.rotorooter.businessservice.rrcontentlayer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class WelcomeController {
+private static final String welcomemsg = "Welcome Mr. %s!";
+    @GetMapping("/welcome")
+    @ResponseBody
+    public Welcome welcomeUser(@RequestParam(name="name", required=false, defaultValue="Java Fan") String name) {
+        return new Welcome(String.format(welcomemsg, name));
+    }
+}
