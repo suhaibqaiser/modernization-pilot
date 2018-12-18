8e509d199286009bdf2942350c5e91f746abfbb1
Saeed Alam <saeedalam@Igors-MBP.architech.lan>
2018-12-18 15:55:48 -0500

    Redis spring config

--- rr-content-layer/src/main/java/com/rotorooter/businessservice/rrcontentlayer/RrContentLayerApplication.java
+++ rr-content-layer/src/main/java/com/rotorooter/businessservice/rrcontentlayer/RrContentLayerApplication.java
@@ -2,12 +2,19 @@ package com.rotorooter.businessservice.rrcontentlayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableConfigurationProperties
 @SpringBootApplication
 public class RrContentLayerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RrContentLayerApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(RrContentLayerApplication.class, args);
+
 	}
 
 }
